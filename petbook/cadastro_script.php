6d0b766b571fbf2f81a1d7c07c6173b11e9e47ad
0italo0 <100356333+0italo0@users.noreply.github.com>
2022-03-30 21:27:52 -0300

    Deletei o html que não precisava

--- petbook/cadastro_script.php
+++ petbook/cadastro_script.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-
 <?php
 //make a method to do a insert in database, taking the post from the form (cadastro.php)
 
@@ -33,7 +22,5 @@
     }
 
 ?>
-</body>
-</html>
 
 
